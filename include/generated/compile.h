384066dd5d05e5b04e2e8f4e184b814c191f621a
Tom Marshall <tdm.code@gmail.com>
2012-04-16 15:04:38 -0700

    Delete some unused crap.

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 1 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Wed Mar 7 13:42:04 PST 2012"
-#define LINUX_COMPILE_TIME "13:42:04"
-#define LINUX_COMPILE_BY "tommy"
-#define LINUX_COMPILE_HOST "pong"
-#define LINUX_COMPILE_DOMAIN "pdsea.f5net.com"
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
